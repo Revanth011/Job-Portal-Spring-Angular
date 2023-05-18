3ed13181c920f1904fbcb430eb2e30ec702fb49f
Revanth011 <83944100+Revanth011@users.noreply.github.com>
2023-05-18 16:26:35 +0530

    Delete jobPortalBackend directory

--- jobPortalBackend/src/main/java/com/example/demo/persistance/AppliedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.persistance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Applied;
-
-public interface AppliedRepository extends JpaRepository<Applied, Integer> {
-
-}
--- jobPortalBackend/src/main/java/com/example/demo/persistance/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.persistance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
